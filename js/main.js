d1e7ff11d0f87b9d393be8d3fb765547df175015
Dave Guarino <dave@codeforamerica.org>
2014-08-27 19:45:15 -0700

    New selected icon

--- js/main.js
+++ js/main.js
@@ -103,8 +103,8 @@ var buildingIcon = L.icon({iconUrl: 'img/falcon_map_marker@1x.png',
                            //iconSize: [31, 41],
                            iconSize: sizeForAllIcons});
                            //iconAnchor: [14, 41]});
-var iconActive = L.icon({iconUrl: 'img/falcon_map_marker_active2@1x.png',
-                                 iconRetinaUrl: 'img/falcon_map_marker_active2@2x.png',
+var iconActive = L.icon({iconUrl: 'img/falcon_map_marker_active@1x.png',
+                                 iconRetinaUrl: 'img/falcon_map_marker_active@2x.png',
                                  //iconSize: [31, 41],
                                  iconSize: sizeForAllIcons});
                                  //iconAnchor: [14, 41]});
